5a2dcabb2eaf242b0968777f84df11844870075d
Fabrice Marsaud <fabrice.marsaud@vates.fr>
2015-12-21 12:38:06 +0100

    Fixed constructor extension

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import Bluebird from 'bluebird'
 import forEach from 'lodash.foreach'
 import isFunction from 'lodash.isfunction'
 
-export default class Smb2Promise extends Smb2 {}
+export default class Smb2Promise extends Smb2 {
+  constructor (...args) {
+    super()
+    Smb2.apply(this, args)
+  }
+}
 
 forEach(Smb2.prototype, (fn, name) => {
   if (isFunction(fn)) {
     Smb2Promise.prototype[name] = Bluebird.promisify(fn)
   }
 })
-
